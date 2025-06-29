ce8babfbc999a0907e4712f30317009fb24fafff
sparshmajotra <majotrasparsh987@gmail.com>
2025-06-29 14:47:47 +0530

    fix: corrected User model import and server.js changes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const User = require("../models/user");
+const user = require("../models/user");
 const bcrypt = require("bcrypt");
 
 // set up multer for profile photo
